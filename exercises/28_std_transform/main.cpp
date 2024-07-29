39c738a3edd98c00c5e39fbc0149a6524bb05a5d
YangYiming <2651575456@qq.com>
2024-07-30 00:03:04 +0800

    28

--- exercises/28_std_transform/main.cpp
+++ exercises/28_std_transform/main.cpp
@@ -5,11 +5,17 @@
 
 // READ: `std::transform` <https://zh.cppreference.com/w/cpp/algorithm/transform>
 // READ: `std::vector::begin` <https://zh.cppreference.com/w/cpp/container/vector/begin>
+std::string mul(int a)
+{
+    return std::to_string(a*2);
+}
 
 int main(int argc, char **argv) {
     std::vector<int> val{8, 13, 21, 34, 55};
     // TODO: 调用 `std::transform`，将 `v` 中的每个元素乘以 2，并转换为字符串，存入 `ans`
     // std::vector<std::string> ans
+    std::vector<std::string> ans(val.size());
+    std::transform(val.begin(),val.end(),ans.begin(),mul);
     ASSERT(ans.size() == val.size(), "ans size should be equal to val size");
     ASSERT(ans[0] == "16", "ans[0] should be 16");
     ASSERT(ans[1] == "26", "ans[1] should be 26");
