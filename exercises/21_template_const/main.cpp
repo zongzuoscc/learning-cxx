6bad990671cc9b0265aa5db019abb4fbc2c0c6a1
YangYiming <2651575456@qq.com>
2024-07-31 23:17:55 +0800

    21

--- exercises/21_template_const/main.cpp
+++ exercises/21_template_const/main.cpp
@@ -1,6 +1,5 @@
 ﻿#include "../exercise.h"
 #include <cstring>
-
 // READ: 模板非类型实参 <https://zh.cppreference.com/w/cpp/language/template_parameters#%E6%A8%A1%E6%9D%BF%E9%9D%9E%E7%B1%BB%E5%9E%8B%E5%AE%9E%E5%8F%82>
 
 template<unsigned int N, class T>
@@ -10,9 +9,12 @@ struct Tensor {
 
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
-        // TODO: 填入正确的 shape 并计算 size
+        for (unsigned int i = 0; i < N; ++i) {
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
-        std::memset(data, 0, size * sizeof(T));
+        std::memset(data, 0, size * sizeof(T)); 
     }
     ~Tensor() {
         delete[] data;
@@ -32,10 +34,12 @@ struct Tensor {
 private:
     unsigned int data_index(unsigned int const indices[N]) const {
         unsigned int index = 0;
-        for (unsigned int i = 0; i < N; ++i) {
-            ASSERT(indices[i] < shape[i]);
-            // TODO: 计算 index
+        unsigned int multiplier = 1;
+        for (int i = N - 1; i >= 0; --i) {
+            index += indices[i] * multiplier;
+            multiplier *= shape[i];
         }
+        return index;
     }
 };
 
