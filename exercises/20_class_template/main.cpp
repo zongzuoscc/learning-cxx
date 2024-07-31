67e2c43242f38268fca45d7c72e2f6003ba2f383
YangYiming <2651575456@qq.com>
2024-07-31 23:12:00 +0800

    20 again

--- exercises/20_class_template/main.cpp
+++ exercises/20_class_template/main.cpp
@@ -1,5 +1,5 @@
 ﻿#include "../exercise.h"
-
+#include <cstring>
 // READ: 类模板 <https://zh.cppreference.com/w/cpp/language/class_template>
 
 template<class T>
@@ -8,66 +8,50 @@ struct Tensor4D {
     T *data;
 
     Tensor4D(unsigned int const shape_[4], T const *data_) {
+        std::copy(shape_, shape_ + 4, shape);
         unsigned int size = 1;
-        // TODO: 填入正确的 shape 并计算 size
-        for(int i=0;i<4;i++)
-        {
-            shape[i]=shape_[i];
-            size *=shape_[i];
+        for (unsigned int i = 0; i < 4; ++i) {
+            size *= shape[i];
         }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
+
     ~Tensor4D() {
         delete[] data;
     }
 
-    // 为了保持简单，禁止复制和移动
     Tensor4D(Tensor4D const &) = delete;
     Tensor4D(Tensor4D &&) noexcept = delete;
 
-    // 这个加法需要支持“单向广播”。
-    // 具体来说，`others` 可以具有与 `this` 不同的形状，形状不同的维度长度必须为 1。
-    // `others` 长度为 1 但 `this` 长度不为 1 的维度将发生广播计算。
-    // 例如，`this` 形状为 `[1, 2, 3, 4]`，`others` 形状为 `[1, 2, 1, 4]`，
-    // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
-        // TODO: 实现单向广播的加法
-         for (int i = 0; i < 4; ++i) {
-            if (shape[i] != others.shape[i] && others.shape[i] != 1) {
-                throw std::invalid_argument("Shapes do not match for broadcasting");
-            }
+        unsigned int this_size = 1;
+        for (unsigned int i = 0; i < 4; ++i) {
+            this_size *= shape[i];
         }
 
-        unsigned int size = 1;
-        for (int i = 0; i < 4; ++i) {
-            size *= shape[i];
-        }
-
-        for (unsigned int i = 0; i < size; ++i) {
-            int idx[4];
-            int temp = i;
+        for (unsigned int i = 0; i < this_size; ++i) {
+            unsigned int idx[4];
+            unsigned int temp = i;
             for (int j = 3; j >= 0; --j) {
                 idx[j] = temp % shape[j];
                 temp /= shape[j];
             }
 
-            int broadcastIdx[4];
+            unsigned int other_idx = 0;
             for (int j = 0; j < 4; ++j) {
-                broadcastIdx[j] = (others.shape[j] == 1) ? 0 : idx[j];
+                other_idx *= others.shape[j] == 1 ? 1 : shape[j];
+                other_idx += (others.shape[j] == 1) ? 0 : idx[j];
             }
 
-            unsigned int otherIdx = broadcastIdx[0];
-            for (int j = 1; j < 4; ++j) {
-                otherIdx = otherIdx * others.shape[j] + broadcastIdx[j];
-            }
-
-            data[i] += others.data[otherIdx];
+            data[i] += others.data[other_idx];
         }
 
         return *this;
     }
 };
+
+
 
 // ---- 不要修改以下代码 ----
 int main(int argc, char **argv) {
@@ -86,7 +70,7 @@ int main(int argc, char **argv) {
         auto t0 = Tensor4D(shape, data);
         auto t1 = Tensor4D(shape, data);
         t0 += t1;
-        for (auto i = 0u; i < sizeof(data) / sizeof(*data); ++i) {
+        for (unsigned int i = 0; i < sizeof(data) / sizeof(int); i++) {
             ASSERT(t0.data[i] == data[i] * 2, "Tensor doubled by plus its self.");
         }
     }
@@ -117,7 +101,7 @@ int main(int argc, char **argv) {
         auto t0 = Tensor4D(s0, d0);
         auto t1 = Tensor4D(s1, d1);
         t0 += t1;
-        for (auto i = 0u; i < sizeof(d0) / sizeof(*d0); ++i) {
+        for (unsigned int i = 0; i < sizeof(d0) / sizeof(int); i++) {
             ASSERT(t0.data[i] == 7.f, "Every element of t0 should be 7 after adding t1 to it.");
         }
     }
@@ -139,7 +123,7 @@ int main(int argc, char **argv) {
         auto t0 = Tensor4D(s0, d0);
         auto t1 = Tensor4D(s1, d1);
         t0 += t1;
-        for (auto i = 0u; i < sizeof(d0) / sizeof(*d0); ++i) {
+        for (unsigned int i = 0; i < sizeof(d0) / sizeof(double); i++) {
             ASSERT(t0.data[i] == d0[i] + 1, "Every element of t0 should be incremented by 1 after adding t1 to it.");
         }
     }
